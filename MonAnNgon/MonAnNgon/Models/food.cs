5811408ce48ed0a477e126080c0061b636206090
quangdang <quangdang@lecle.co.kr>
2021-12-11 17:13:46 +0700

    feat: :sparkles: add home page

---
+++ MonAnNgon/MonAnNgon/Models/food.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonAnNgon.Models
+{
+    public class food
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string img { get; set; }
+    }
+}
--- MonAnNgon/MonAnNgon/Views/HomePage.xaml.cs
+++ MonAnNgon/MonAnNgon/Views/HomePage.xaml.cs
@@ -6,15 +6,32 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using MonAnNgon.Models;
+using System.Collections.ObjectModel;
 
 namespace MonAnNgon.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+
+        public ObservableCollection<food> ItemsLayout { get; set; }
+
         public HomePage()
         {
             InitializeComponent();
+            ItemsLayout = new ObservableCollection<food>
+            {
+                new food { id = "1", name = "Bánh", img = "CanhThinhDuDu.jpg" },
+                new food { id = "2", name = "Chiên Xào", img = "green_spinach.jpg" },
+                new food { id = "3", name = "Nướng", img = "ot_chuong_hai_san.jpg" },
+                new food { id = "4", name = "Kho", img = "salad.jpg" },
+                new food { id = "4", name = "Kho", img = "salad.jpg" },
+                new food { id = "2", name = "Chiên Xào", img = "green_spinach.jpg" },
+                new food { id = "3", name = "Nướng", img = "ot_chuong_hai_san.jpg" },
+                new food { id = "4", name = "Kho", img = "salad.jpg" },
+            };
+            BindingContext = this;
         }
     }
 }
