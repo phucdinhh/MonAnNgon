c8f2c25405bc0248ec065a911d7b781f623987d7
quangdang <quangdang@lecle.co.kr>
2021-12-18 20:31:05 +0700

    fix: :bug: search page

--- MonAnNgon/MonAnNgon/Views/SearchPage.xaml.cs
+++ MonAnNgon/MonAnNgon/Views/SearchPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using MonAnNgon.Models;
 
 namespace MonAnNgon.Views
 {
@@ -15,6 +16,43 @@ namespace MonAnNgon.Views
         public SearchPage()
         {
             InitializeComponent();
+            MyListView.ItemsSource = GetList();
+        }
+
+        private static IEnumerable<Category> GetList(string searchText = null)
+        {
+            List<Category> contacts = new List<Category>
+            {
+               new Category { id = "1", name = "Bánh", img = "CanhThinhDuDu.jpg" },
+                new Category { id = "2", name = "Chiên Xào", img = "green_spinach.jpg" },
+                new Category { id = "3", name = "Nướng", img = "ot_chuong_hai_san.jpg" },
+                new Category { id = "4", name = "Kho", img = "salad.jpg" },
+            };
+
+            return string.IsNullOrEmpty(searchText) ? contacts : contacts
+                .Where(c => c.name.ToLower()
+                .StartsWith(searchText.ToLower()));
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            MyListView.ItemsSource = GetList(e.NewTextValue);
+        }
+
+        private void MyListView_Refreshing(object sender, EventArgs e)
+        {
+            MyListView.ItemsSource = GetList();
+            MyListView.EndRefresh();
+        }
+
+        private void MyListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item is Category contact) DisplayAlert("Following", contact.name, "Ok", "Cancel");
+        }
+
+        private void MyListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            MyListView.SelectedItem = null;
         }
     }
 }
